3674c94105f0dfd17d11a877925ff0b8d0d5d6d1
liaobinloves <33417791+liaobinloves@users.noreply.github.com>
2019-11-30 21:29:26 +0800

    Add files via upload

---
+++ parameter.py
@@ -0,0 +1,13 @@
+from math import pi
+
+class Constant:
+    '''
+    In units of cgs.
+    '''
+    Msun = 1.9891e33   # mass of Sun, in units of g
+    G = 6.67259e-8  # constant of gravity, cm^3/(s^2g)
+    Mmilky = 4.31e6*Msun # mass of Milky, g
+    c = 29979245800. # light speed, cm/s
+    au = 14959787070000. # distance between Sun and Earth, cm
+    pc = au*360*60*60/(2*pi) # 1 parsecond, cm
+        
